a8d1eb9975f535a5730a8b5153969ce75d10433d
hakim112211 <maulanahakimrizki@gmail.com>
2023-07-31 14:04:51 +0700

    menambahkan video 16

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
 const app = express()
 const port = 3000;
-
 // gunakan ejs
 app.set('view engine', 'ejs');
 
+// Third-party Middleware
+app.use(expressLayouts);
+app.use(morgan('dev'));
+
+// Built-in middleware
+app.use(express.static('public'));
+
+
+// application level middleware
+app.use((req, res, next) => {
+	console.log('Time: ', Date.now());
+	next();  
+});
+
+// application level middleware
+app.use((req, res, next) => {
+	console.log('ini middleware ke-2');
+	next();  
+});
+
+
 app.get('/', (req, res) => {
-	// res.sendFile('./index.html', { root: __dirname});
 	const mahasiswa = [
 		{
 			nama: 'Laurientus Rando',
@@ -48,7 +68,7 @@ app.get('/product/:id', (req, res) => {
 	res.send(`Product ID : ${req.params.id} <br> Category : ${req.query.category}`);
 });
 
-app.use('/', (req, res) => {
+app.use((req, res) => {
 	res.status(404);
 	res.send('<h1>404 not found</h1>');
 });
